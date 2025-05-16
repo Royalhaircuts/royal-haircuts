84e16263df6f6bb896decc0889c316ffb4b67fe6
Royalhaircuts <otuoacheampongoheneba28@gmail.com>
2025-05-16 11:46:58 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+lucide.createIcons();
+
+document.getElementById('appointment-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Appointment successfully booked!');
+});
